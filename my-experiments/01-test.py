71daab320ca26813b53faed076a4309d5d35162e
caoxiang <adamcx@foxmail.com>
2024-06-10 23:49:43 +0800

    注意力基础代码 验证代码

--- my-experiments/01-test.py
+++ my-experiments/01-test.py
@@ -7,7 +7,7 @@
 from ultralytics import YOLO
 
 # Load a model
-model = YOLO('yolov8s.yaml')  # load a pretrained model (recommended for training)
+model = YOLO('yolov8s.yaml').load("yolov8s.pt")  # load a pretrained model (recommended for training)
 
 # Train the model with 2 GPUs
-results = model.train(data='/data/cx/datasets/yolo-exp-etl-data/data.yaml', imgsz=640, device='1', save_dir='/data/cx/ysp-2024/yolov8-exp/runs', optimizer='Adam', lr0=0.001)
+results = model.train(data='/data/cx/datasets/yolo-exp-etl-data/data.yaml', imgsz=640, device='1', save_dir='/data/cx/ysp-2024/yolov8-exp/runs')
