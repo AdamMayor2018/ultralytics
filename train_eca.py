8ab9e1f47aaaba60dc13b6ff18a883c3791ae09b
caoxiang <adamcx@foxmail.com>
2024-07-02 23:29:35 +0800

    修改默认baseline的yolo yaml文件bug 新增注意力相关实验的脚本

---
+++ train_eca.py
@@ -0,0 +1,8 @@
+from ultralytics import YOLO
+
+# Load a model
+model = YOLO("yolov8-eca.yaml").load("yolov8s.pt")  # build from YAML and transfer weights
+
+# Train the model
+results = model.train(data="etl.yaml", epochs=100, imgsz=640, name='train_v8s_eca')
+
