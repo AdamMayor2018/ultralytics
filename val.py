f944ccd2ccc2fca5f58556fdb6698fb1231252c9
caoxiang <adamcx@foxmail.com>
2024-07-08 23:14:06 +0800

    1.删除部分没用的配置文件 2.修改val脚本代码

--- val.py
+++ val.py
@@ -1,16 +1,12 @@
 from ultralytics import YOLO
 import argparse
-
-
-
-parser = argparse.ArgumentParser()
-parser.add_argument('--exp_name', default='train_v8s_baseline', type=str, help='opt train_v8s_baseline, yolo8_s5_modify_DCN_100, yolo8_s45_modify_DCN, yolo8_s345_modify_DCN, yolo8_s2345_modify_DCN, yolo8_merge_ep150_label_smoothing_0.1, yolo8_DFPN_ep_60_smootihing_0.2')
-parser.add_argument('--mode', default='val', type=str, choices=['val', 'test'])
-
-
-
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--exp_name', default='train_v8s_baseline', type=str,
+                        help='name of the experiment')
+    parser.add_argument('--mode', default='val', type=str, choices=['val', 'test'])
+    parser.add_argument('--data', default='ultralytics/cfg/datasets/etl.yaml', type=str, help='data yaml path')
     args = parser.parse_args()
     print('processing : {}'.format(args.exp_name))
     model = YOLO("runs/detect/{}/weights/best.pt".format(args.exp_name)) 
-    results = model.val(data="etl.yaml", imgsz=640, batch=16, conf=0.25, iou=0.6, device="0", split=args.mode)
+    results = model.val(data=args.data, imgsz=640, batch=16, conf=0.25, iou=0.6, device="0", split=args.mode)
