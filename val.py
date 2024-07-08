ce74cfeb465b7cc15665f4687d57a210ac6d3797
dpxudong <854044512@qq.com>
2024-07-08 22:34:49 +0800

    update

--- val.py
+++ val.py
@@ -4,7 +4,7 @@ import argparse
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--exp_name', default='train_v8s_baseline', type=str, help='opt train_v8s_baseline, yolo8_s5_modify_DCN_100, yolo8_s45_modify_DCN, yolo8_s345_modify_DCN, yolo8_s2345_modify_DCN, yolo8_merge_ep150_label_smoothing_0.1, yolo8_s45_modify_DCN')
+parser.add_argument('--exp_name', default='train_v8s_baseline', type=str, help='opt train_v8s_baseline, yolo8_s5_modify_DCN_100, yolo8_s45_modify_DCN, yolo8_s345_modify_DCN, yolo8_s2345_modify_DCN, yolo8_merge_ep150_label_smoothing_0.1, yolo8_DFPN_ep_60_smootihing_0.2')
 parser.add_argument('--mode', default='val', type=str, choices=['val', 'test'])
 
 
